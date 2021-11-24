67d6ec1e57c6e49d905590112ad504b39f282e40
이병헌 <brian52125@gmail.com>
2021-11-25 01:21:56 +0900

    인벤토리 프리팹 적용
    인벤토리 프리팹을 가져와서 적용시키고 아이템을 만들어서 작동 확인함

---
+++ Assets/HOJIN/Scripts/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ItemTypeList //아이템 종류 리스트
+{
+    None,
+    Circle,
+    Heart,
+    Note,
+    Key_1
+}
+
+[System.Serializable]
+public class Item
+{
+    public ItemTypeList ItemType;
+    public Sprite ItemImage;
+}
